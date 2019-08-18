fa0ab7ed2026e10f5163b757e677f0c5ee2b9e80
tdawg111 <tripletdawg111@gmail.com>
2019-08-18 23:00:53 +0200

    Convert from web app to console app. Add logging dependency.

--- src/main/java/feed/twitter/simulate/main/Application.java
+++ src/main/java/feed/twitter/simulate/main/Application.java
@@ -1,5 +1,7 @@
 package feed.twitter.simulate.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,9 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
+
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -17,12 +22,12 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            log.info("Let's inspect the beans provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+                log.info(beanName);
             }
         };
     }
